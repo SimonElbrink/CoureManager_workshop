61d962ce7a0bdf0f4cc4e18e8b79175ca19a662f
Simon Elbrink <simon.elbrink@gmail.com>
2022-01-20 15:30:28 +0100

    Part 2 REPOS DONE!

--- src/main/java/se/lexicon/course_manager_assignment/data/dao/CourseCollectionRepository.java
+++ src/main/java/se/lexicon/course_manager_assignment/data/dao/CourseCollectionRepository.java
@@ -2,7 +2,9 @@ package se.lexicon.course_manager_assignment.data.dao;
 
 
 
+import se.lexicon.course_manager_assignment.data.sequencers.CourseSequencer;
 import se.lexicon.course_manager_assignment.model.Course;
+import se.lexicon.course_manager_assignment.model.Student;
 
 import java.time.LocalDate;
 import java.util.Collection;
@@ -20,42 +22,91 @@ public class CourseCollectionRepository implements CourseDao{
 
     @Override
     public Course createCourse(String courseName, LocalDate startDate, int weekDuration) {
-        return null;
+
+        int id = CourseSequencer.nextCourseId();
+        Course course = new Course(id,courseName,startDate,weekDuration);
+
+        //Ternary Operator
+       return courses.add(course) ? course : null;
+
+
     }
 
     @Override
     public Course findById(int id) {
+        for (Course course : courses) {
+            if (course.getId().equals(id)){
+                return course;
+            }
+        }
         return null;
     }
 
     @Override
     public Collection<Course> findByNameContains(String name) {
-        return null;
+
+        Collection<Course> matchingCourses = new HashSet<>();
+
+        for (Course course: courses) {
+            if (course.getCourseName().toLowerCase().trim().contains(name.trim().toLowerCase())){
+                matchingCourses.add(course);
+            }
+        }
+
+        return matchingCourses;
     }
 
     @Override
     public Collection<Course> findByDateBefore(LocalDate end) {
-        return null;
+
+        Collection<Course> found = new HashSet<>();
+
+        for (Course course :
+                courses) {
+            if (course.getStartDate().isBefore(end)){
+                found.add(course);
+            }
+        }
+        return found;
     }
 
     @Override
     public Collection<Course> findByDateAfter(LocalDate start) {
-        return null;
+        Collection<Course> found = new HashSet<>();
+
+
+        for (Course c : courses) {
+            if (c.getStartDate().isAfter(start)) {
+                found.add(c);
+            }
+        }
+
+        return found;
     }
 
     @Override
     public Collection<Course> findAll() {
-        return null;
+        return new HashSet<>(courses);
     }
 
     @Override
     public Collection<Course> findByStudentId(int studentId) {
-        return null;
+
+        Collection<Course> found = new HashSet<>();
+
+        for (Course course : courses){
+            for (Student student: course.getStudents()){
+                if (student.getId().equals(studentId)){
+                    found.add(course);
+                }
+            }
+        }
+        return found;
     }
 
     @Override
     public boolean removeCourse(Course course) {
-        return false;
+        return courses.remove(course);
     }
 
     @Override
